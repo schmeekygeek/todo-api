a61de8287394159187c355e401d8da8039c386f7
schmeekygeek <ss6087982@gmail.com>
2022-05-12 14:26:53 +0530

    added custom exception handler ResourceNotFoundException

--- src/main/java/io/schmeekydev/todoApp/exceptions/ResourceNotFoundException.java
+++ src/main/java/io/schmeekydev/todoApp/exceptions/ResourceNotFoundException.java
@@ -4,6 +4,7 @@ public class ResourceNotFoundException extends RuntimeException {
 
     String resourceName;
     String fieldName;
+    String keyword;
     long fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue){
@@ -11,5 +12,11 @@ public class ResourceNotFoundException extends RuntimeException {
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
+    }
+    public ResourceNotFoundException(String resourceName, String fieldName, String keyword){
+        super(String.format("%s not found with %s : %s", resourceName, fieldName, keyword));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.keyword = keyword;
     }
 }
