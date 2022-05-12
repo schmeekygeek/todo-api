76d3929ae6518cfae3a6aabef89675c26de02858
schmeekygeek <ss6087982@gmail.com>
2022-05-12 14:25:20 +0530

    mapped task to user

--- src/main/java/io/schmeekydev/todoApp/entities/Task.java
+++ src/main/java/io/schmeekydev/todoApp/entities/Task.java
@@ -9,12 +9,14 @@ import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Task {
 
     @Id
--- src/main/java/io/schmeekydev/todoApp/repositories/TaskRepository.java
+++ src/main/java/io/schmeekydev/todoApp/repositories/TaskRepository.java
@@ -1,9 +1,16 @@
 package io.schmeekydev.todoApp.repositories;
 
+import io.schmeekydev.todoApp.entities.Task;
+import io.schmeekydev.todoApp.entities.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import io.schmeekydev.todoApp.entities.Task;
-
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Integer>{}
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+	List<Task> findByUser(User user);
+
+	// @Query(value = "SELECT t FROM task WHERE t.title like '%?1%'")
+	// List<Task> searchTaskTitleByKeyword(String keyword);
+}
